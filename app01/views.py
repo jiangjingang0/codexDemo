96e1ac20e50d6f1c3c56de27e7d3e33bd9e76b6d
JIANG <1920419411@qq.com>
2023-02-27 09:53:28 +0800

    更新

--- app01/views.py
+++ app01/views.py
@@ -12,18 +12,29 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 # Create your views here.
 
 def index(request):
-    q = json.loads(request.GET.get("question"))
+    print(request.GET['question'])
+    q = request.GET['question']
 
     response = openai.Completion.create(
         model="code-davinci-002",
-        prompt=q['content'],
+        prompt=q,
         temperature=0,
         max_tokens=256,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
     )
+    print(response)
+    oldStr = response['choices'][0].text
+    print(oldStr)
+    newStr = oldStr.replace("\n+", "\n")
+    print(newStr)
     data = {
-        "content": response['choices'][0].text
+        "content": newStr
     }
+
     return HttpResponse(json.dumps(data))
+
+
+def test(request):
+    return render(request, "index.html")
--- codexDemo/urls.py
+++ codexDemo/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from app01 import views
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.index)
+    path("", views.index),
+    path("test/", views.test)
 ]
